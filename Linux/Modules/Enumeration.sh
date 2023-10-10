669c259d84de5a123f4eec786625b68aeb20eae5
zachrousseau <zachrousseau0@gmail.com>
2023-10-10 16:57:28 -0400

    Very Rough Linux functions

---
+++ Linux/Modules/Enumeration.sh
@@ -0,0 +1,24 @@
+get_admins{
+    adminGroups=$()
+    tempadm=$(cat /etc/group | grep adm)
+    adminGroups+=${tempadm##*:}
+    tempadm=$(cat /etc/group | grep sudo)
+    adminGroups+=${tempadm##*:}
+    tempadm+=$(cat /etc/group | grep admin)
+    adminGroups+=${tempadm##*:}
+
+    $admins
+}
+
+get_installed_programs{
+    dnf list installed || yum list installed | apt list --installed 
+}
+
+get_locallisteningports{
+    $listeningports=$(sudo netstat -tunlp)
+}
+
+get_services{
+    systemctl --type=service --state=running
+}
+
---
+++ Linux/Modules/Tools.sh
@@ -0,0 +1,3 @@
+update_all{
+    sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+}
