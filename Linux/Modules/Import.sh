026043696b269ce339bcdb51f2ef9902ce66ee53
zachrousseau <zachrousseau0@gmail.com>
2023-10-13 23:13:40 -0400

    Update Import file

--- Linux/Modules/Import.sh
+++ Linux/Modules/Import.sh
@@ -2,9 +2,15 @@
 
 echo "Loading Custom Functions"
 
-#  Run in the bash shell to autoload the commands each time you open the bash shell
-# echo "source '/mnt/hgfs/Github - CIC Scripts/CIC-Scripts/Linux/Modules/Import.sh'" | tee -a ~/.bashrc
+: <<'END'
+Run in the bash shell to autoload the commands each time you open the bash shell. Run the command below.
+Change the path to where ever the Modules folder is stored 
 
+echo "source '/mnt/hgfs/Github - CIC Scripts/CIC-Scripts/Linux/Modules/Import.sh'" | tee -a ~/.bashrc
 
-source "/mnt/hgfs/Github - CIC Scripts/CIC-Scripts/Linux/Modules/Enumeration.sh"
-source "/mnt/hgfs/Github - CIC Scripts/CIC-Scripts/Linux/Modules/Tools.sh"
+END
+
+$PATH = "/mnt/hgfs/Github - CIC Scripts/CIC-Scripts/Linux/Modules"
+
+source "$PATH/Enumeration.sh"
+source "$PATH/Tools.sh"
