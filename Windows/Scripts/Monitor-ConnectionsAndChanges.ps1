618a676740b5fcbd674456122370137aede037a0
Orlando Yeo <orlandoyeo@gmail.com>
2024-01-14 15:36:16 -0700

    [PREP] easier ssh monitor

--- Windows/Scripts/Monitor-ConnectionsAndChanges.ps1
+++ Windows/Scripts/Monitor-ConnectionsAndChanges.ps1
@@ -1,16 +1,7 @@
 Write-Host "Starting monitoring on $($env:COMPUTERNAME)"
 
 while ($true) {
-    # Values to monitor
     $initialSSHValue = Get-CimInstance -ClassName Win32_Process -Filter "Name = 'sshd.exe'" | Get-CimAssociatedInstance -Association Win32_SessionProcess | Get-CimAssociatedInstance -Association Win32_LoggedOnUser | Where-Object {$_.Name -ne 'SYSTEM'}
+    Write-Warning "$($initialSSHValue.Name) has connected to $($env:COMPUTERNAME)"
 
-    # Redefining the values we are monitoring
-    $checkSSHValue = Get-CimInstance -ClassName Win32_Process -Filter "Name = 'sshd.exe'" | Get-CimAssociatedInstance -Association Win32_SessionProcess | Get-CimAssociatedInstance -Association Win32_LoggedOnUser | Where-Object {$_.Name -ne 'SYSTEM'}
-
-    # Comparing the two cim instance objects
-    $compare = ((Compare-Object -ReferenceObject $initialSSHValue -DifferenceObject $checkSSHValue).InputObject.Name)
-
-    if ($null -notlike $compare) {
-        Write-Warning "$compare has connected to $($env:COMPUTERNAME)"
-    }
 }
