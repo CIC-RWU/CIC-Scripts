14972025f64a32253328de269a5f39e0e493fd53
Orlando Yeo <orlandoyeo@gmail.com>
2024-01-14 16:13:27 -0700

    [PREP] I made it too complicated, this works

--- Windows/Scripts/Monitor-ConnectionsAndChanges.ps1
+++ Windows/Scripts/Monitor-ConnectionsAndChanges.ps1
@@ -1,7 +1,14 @@
 Write-Host "Starting monitoring on $($env:COMPUTERNAME)"
 
 while ($true) {
+    Start-Sleep 5
     $initialSSHValue = Get-CimInstance -ClassName Win32_Process -Filter "Name = 'sshd.exe'" | Get-CimAssociatedInstance -Association Win32_SessionProcess | Get-CimAssociatedInstance -Association Win32_LoggedOnUser | Where-Object {$_.Name -ne 'SYSTEM'}
-    Write-Warning "$($initialSSHValue.Name) has connected to $($env:COMPUTERNAME)"
-
+    if ($null -ne $initialSSHValue) {
+        $initialSSHValue | ForEach-Object {
+            Write-Warning "$($_.Name) has an active SSH session on $env:COMPUTERNAME"
+        }
+    } else {
+            Write-Host "`n"
+            Continue
+        }
 }
